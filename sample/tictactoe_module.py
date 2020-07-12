24c76c198aa04a64689cff45e19ce02e7d412fc7
sibeov-Laptop <sibeov@outlook.com>
2020-07-12 17:32:53 +0200

    A lot of changes. Methodes removed etc.

--- sample/tictactoe_module.py
+++ sample/tictactoe_module.py
@@ -1,3 +1,4 @@
+import unittest
 class ticTacToe:
     
     def __init__(self, boardDict = None):
@@ -20,11 +21,7 @@ class ticTacToe:
         self.player1Mark = 'X'
         self.player2Mark = 'O'
 
-        self.player1Turn = 0
-        self.player2Turn = 0
-        self.roundNumber = 1
-
-        self.gameVersion = 1.04
+        self.gameVersion = 1.2
 
     def __str__(self):
         return f"A TicTacToe game by s1b30v. v{self.gameVersion}"
@@ -35,11 +32,9 @@ class ticTacToe:
             print(f"|{tttBoard[i-2]}|{tttBoard[i-1]}|{tttBoard[i]}|")
             i -= 3
     
-    # PENDING: Check function.
     def printRoundNumber(self):
         print(f"Round: {self.roundNumber}")
         
-    # IN PROGRESS: markBoard function. Marking the board from input.
     def markBoard(self, playerIp, player):
         while(playerIp in self.allowedNumbers):
             while(player == 1):
@@ -49,37 +44,43 @@ class ticTacToe:
                 self.boardDict[playerIp] = 'O'
                 return 1
         return 0
- 
+
     def removeUnavailableTile(self, playerIp):
         while(playerIp in self.availableNumbers):
             self.availableNumbers.remove(playerIp)
             return 1
         return 0
     
-    # PENDING: Checker functions.
     # PENDING: Check function Columns.
     def checkColumns(self, tttBoard):
         i = 9
-        while(i in self.allowedNumbers):
-            while(tttBoard[i-6] == tttBoard[i-3] == tttBoard[i]):
-                return 1
+        while(i > 6 and i < 10):
+            while(tttBoard[i] == tttBoard[i-3] == tttBoard[i-6]):
+                while(tttBoard[i] == ' '):
+                    return (0, 0)
+                return (1, tttBoard[i])
             i -= 1
-        return 0
+        return (0, 0)
 
     # PENDING: Check function Rows.
     def checkRows(self, tttBoard):
         i = 9
-        while(i in self.allowedNumbers):
-            while(tttBoard[i-2] == tttBoard[i-1] == tttBoard[i]):
-                return 1
-            i -= 1
-        return 0
+        while(i > 2 and i < 10):
+            while(tttBoard[i] == tttBoard[i-1] == tttBoard[i-2]):
+                while(tttBoard[i] == ' '):
+                    return (0, 0)
+                return (1, tttBoard[i])
+            i -= 3
+        return (0, 0)
 
     # PENDING: Check function diagonals.
     def checkDiagonals(self, tttBoard):
-        i = 1
-        while(i in self.allowedNumbers):
-            while(tttBoard[i] == tttBoard[5] == tttBoard[i+6]):
-                return 1
-            i += 3
-        return 0
+        while(tttBoard[1] == tttBoard[5] == tttBoard[9]):
+            while(tttBoard[5] == ' '):
+                return (0, 0)
+            return (1, tttBoard[5])
+        while(tttBoard[3] == tttBoard[5] == tttBoard[7]):
+            while(tttBoard[5] == ' '):
+                return (0, 0)
+            return (1, tttBoard[5])
+        return (0, 0)
--- tictactoe_main.py
+++ tictactoe_main.py
@@ -15,7 +15,7 @@
 from sample import tictactoe_module
 import time
 import pdb
-
+import re
 
 #########################################################################
 #TIMER START:
@@ -25,9 +25,16 @@ startTime = time.perf_counter()
 #########################################################################
 #GLOBAL VARS:
 #########################################################################
+tttGame = tictactoe_module.ticTacToe()
+tttBoard = tttGame.boardDict
+
+playerNum = 1
+result = (0, 0)
+playerInput = "Gibberish"
+
 
 #########################################################################
-#FUNCTIONS DEF:
+#FUNCTIONS DEF: Some helper functions.
 #########################################################################
 def playerSwitcher(playerNumber):
     while(playerNumber <= 1):
@@ -39,21 +46,37 @@ def playerSwitcher(playerNumber):
 #MAIN STARTS HERE:
 #########################################################################
 
-tttGame = tictactoe_module.ticTacToe()
-player = 1
-
 # Main game loop:
 while(True):
-    tttGame.printBoard(tttGame.boardDict)
 
-    playerInput = int(input(f"Player {player} -> Enter number [1 -> 9]: "))
-    while(type(playerInput) != int):
-        playerInput = int(input(f"Player {player} -> Enter number [1 -> 9]: "))
+    # Print board part.
+    tttGame.printBoard(tttBoard)
 
-    tttGame.markBoard(playerInput, player)
-    
-    player = playerSwitcher(player)
+    # Player input part.
+    playerInput = int(input(f"Player{playerNum} input number [1-9] -> "))
+    while(playerInput not in tttGame.availableNumbers):
+        playerInput = int(input(f"Player{playerNum} input number [1-9] -> "))
 
+    # Mark board and remove marked tile.
+    tttGame.markBoard(playerInput, playerNum)
+    tttGame.removeUnavailableTile(playerInput)
+
+    # Check board for three in row.
+    # BUG: Does not detect winner. Scope issue?
+    while(result == (0, 0)):
+        result = tttGame.checkColumns(tttBoard)
+        result = tttGame.checkRows(tttBoard)
+        result = tttGame.checkDiagonals(tttBoard)
+        break
+
+    # Print winner and exit game:
+    while(result != (0, 0)):
+        while(result[1] == 'X'):
+            print(f"Player 1 is the winner!")
+        while(result[1] == 'O'):
+            print(f"Player 2 is the winner!")
+
+    playerNum = playerSwitcher(playerNum)
 
 endTime = time.perf_counter()
 print(f"Runtime: {startTime - endTime}s")
